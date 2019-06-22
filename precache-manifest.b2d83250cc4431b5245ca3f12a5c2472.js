82e1acb1f315528dd784c1a96c6d5bd34630eae2
rohitsancheti96 <rohitsancheti96@gmail.com>
2019-06-22 22:08:26 +0530

    Updates

---
+++ precache-manifest.b2d83250cc4431b5245ca3f12a5c2472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094d78de61c2974b6f056cabd3f8c146",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "bb9d1b232418a6d1830a",
+    "url": "/video-search/static/css/main.5e72b46c.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/video-search/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "bb9d1b232418a6d1830a",
+    "url": "/video-search/static/js/main.5bd48a56.chunk.js"
+  },
+  {
+    "revision": "74833dc51c18c4085545",
+    "url": "/video-search/static/js/runtime~main.301bb524.js"
+  }
+]);
